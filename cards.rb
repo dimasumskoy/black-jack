4fd9de719a9e5d62817497ca6a4271460d6a53b4
Dima <sumskoydima@gmail.com>
2017-05-07 22:59:07 +0300

    Added indexing & refactored

--- cards.rb
+++ cards.rb
@@ -1,29 +1,42 @@
+require_relative 'instance_counter'
+
 class Cards
+  include InstanceCounter
+
   @cards = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 
-  def self.deck
-    @deck ||= cards_to_deck
-  end
-
-  def self.cards_to_deck
-    suits = %w(+ <3 ^ <>)
-    deck = []
-    suits.each do |suit|
-      deck << @cards.collect { |card| card + suit }
+  class << self
+    def deck
+      @deck ||= cards_to_deck
     end
-    deck.flatten!
+
+    def cards_to_deck
+      suits = %w(+ <3 ^ <>)
+      deck = []
+      suits.each do |suit|
+        deck << @cards.collect { |card| card + suit }
+      end
+      deck.flatten!
+    end
+
+    def send_card(card)
+      @deck.delete(card)
+    end
   end
 
   attr_accessor :card, :point
 
   def initialize
-    @card = self.class.deck[rand(0..52)]
+    register_instance
+    @index = self.class.instances
+    @card = self.class.deck[@index - 1]
     count_point
+    reset_instance
   end
 
   def count_point
     if @card.start_with?("J", "Q", "K")
-      @point = 10 
+      @point = 10
     elsif @card.start_with?("A")
       @point = 11
     else
@@ -31,7 +44,9 @@ class Cards
     end
   end
 
-  def send_card(card)
-    self.class.deck.delete(card)
+  def reset_instance
+    if self.class.instances > 51
+      self.class.instances = 0
+    end
   end
 end
