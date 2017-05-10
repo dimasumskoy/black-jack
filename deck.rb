9bbfa194785500e74a6431f14c291b830f7adb0a
Dima <sumskoydima@gmail.com>
2017-05-10 20:56:47 +0300

    Added 'deck' class to app

---
+++ deck.rb
@@ -0,0 +1,29 @@
+require_relative 'cards'
+
+class Deck
+  VALUES = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  SUITS  = %w(+ <3 ^ <>)
+
+  attr_accessor :cards
+
+  def initialize
+    @cards = build_deck
+  end
+
+  def build_deck
+    SUITS.flat_map do |suit|
+      VALUES.collect do |value|
+        Cards.new(value + suit)
+      end
+    end.shuffle!
+  end
+
+  def take_card_from_deck(player)
+    @card = @cards[0]
+    @card.change_ace_point(player)
+
+    player.take_card(@card)
+    @cards.delete(@card)
+    @cards.shuffle!
+  end
+end
