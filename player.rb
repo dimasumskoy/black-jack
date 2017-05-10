70d109b8d87baef7c73046e773fc1d83abdce84f
Dima <sumskoydima@gmail.com>
2017-05-10 21:08:44 +0300

    Added 'show_cards' method

--- player.rb
+++ player.rb
@@ -1,13 +1,25 @@
+require_relative 'cards'
+
 class Player
   attr_accessor :name, :money_amount, :cards, :points_amount
 
   def initialize(*)
-    @money_amount = 100
     @cards = []
+    @money_amount = 100
     @points_amount = 0
   end
 
   def take_card(*cards)
     cards.each { |card| @cards << card }
+  end
+
+  def show_cards
+    player_cards = []
+    @cards.each { |card| player_cards << card.type }
+    if self.name == "Diler"
+      print "Карты дилера: #{player_cards}\n"
+    else
+      print "Карты игрока: #{player_cards}\n"
+    end
   end
 end
